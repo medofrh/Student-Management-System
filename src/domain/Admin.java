5c4a5c30f6119fab01ae15e1291e011043248aa3
medofrh <info@medo98.com>
2025-11-11 16:40:35 +0100

    Add Admin class for user management and system administration

---
+++ src/domain/Admin.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Admin extends User {
+
+    public Admin(String username, String password) {
+        super(username, password);
+    }
+
+    public void manageUsers() {
+        // Logic for managing users
+    }
+
+    public void manageCourses() {
+        // Logic for managing courses
+    }
+
+    public void manageSystemData() {
+        // Logic for managing system data
+    }
+}
