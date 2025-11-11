55d836a95cf91905c3e52787faa177a594618bee
medofrh <info@medo98.com>
2025-11-11 16:44:54 +0100

    Moved DatabaseManager from domain to service package

---
+++ src/domain/DatabaseManager.java
@@ -0,0 +1,24 @@
+package domain;
+
+import java.util.List;
+
+public class DatabaseManager {
+
+    public void save(Object object) {
+        // Logic to save an object to the storage system
+    }
+
+    public Object retrieve(String id, Class<?> clazz) {
+        // Logic to retrieve an object by ID and type
+        return null;
+    }
+
+    public List<Object> retrieveAll(Class<?> clazz) {
+        // Logic to retrieve all objects of a specific type
+        return null;
+    }
+
+    public void update(Object object) {
+        // Logic to update an object in the storage system
+    }
+}
